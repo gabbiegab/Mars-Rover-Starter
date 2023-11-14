ac5bd9147a7a50b7dd865f9be1af80b9cc032436
Gabrielle <gabbiehull@gmail.com>
2023-11-14 15:29:09 -0600

    Late but on time Rover

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -1,12 +1,23 @@
 const Command = require('../command.js');
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
+// //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Command class", function() {
 
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  
+  it("constructor sets command type", function() {
+    const newCommand = new Command("MOVE", 100); 
+    expect (newCommand.commandType).toBe("MOVE")  
+  });
 
-});
+  it("constructor sets a value passed in as the 2nd argument", function() {
+    const newCommand = new Command("MOVE", 100); 
+    expect (newCommand.value).toBe(100)  
+  });
+})
+
+

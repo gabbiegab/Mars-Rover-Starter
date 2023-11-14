a45981533d360d9f282843417bc3657d2937fbcd
Gabrielle <gabbiehull@gmail.com>
2023-11-14 15:59:43 -0600

    Added test for Message

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -4,6 +4,21 @@ const Command = require('../command.js');
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Message class", function() {
+describe("Message class", function() { 
+    it("throws error if a name is NOT passed into the constructor as the first parameter", function(){
+        expect( function() { new Message();}).toThrow(new Error('name required'));
+    })
+    it("constructor sets name", function(){
+      const newMessage = new Message("name"); 
+    expect (newMessage.name).toBe("name") 
+
+    })
+    it("contains a commands array passed into the constructor as the 2nd argument", function(){
+        const commands = [new Command("MOVE", 100)]
+        const newMessage = new Message("Roven", commands)
+      expect (newMessage.commands).toBe(commands) 
+      
+  
+      })
 
 });
